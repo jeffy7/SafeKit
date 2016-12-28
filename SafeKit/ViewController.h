559edf9e214a9d4b2c15d99a37d280735e0c3b66
je_ffy <jzxiaobao@gmail.com>
2016-12-28 15:22:20 +0800

    Initial Commit

---
+++ SafeKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeKit
+//
+//  Created by je_ffy on 2016/12/28.
+//  Copyright © 2016年 je_ffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SafeKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SafeKit
+//
+//  Created by je_ffy on 2016/12/28.
+//  Copyright © 2016年 je_ffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
