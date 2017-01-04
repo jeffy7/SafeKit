c1b6ffd83cb7be8241603d3e6b31413911e7eeb9
je_ffy <jzxiaobao@gmail.com>
2017-01-04 23:03:54 +0800

    Runtime 实战之防止重复点击

---
+++ SafeKit/UIButton+RepeatClick.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+RepeatClick.h
+//  SafeKit
+//
+//  Created by je_ffy on 2017/1/4.
+//  Copyright © 2017年 je_ffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (RepeatClick)
+
+@property (nonatomic, assign) NSTimeInterval sp_acceptEventInterval;
+//@property (nonatomic, assign) BOOL sp_ignoreEvent;
+@end
--- SafeKit/ViewController.m
+++ SafeKit/ViewController.m
@@ -19,8 +19,21 @@
     NSString *atr = nil;
     NSMutableArray *array = [[NSMutableArray alloc] init];
     [array addObject:atr];
+    
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    button.backgroundColor = [UIColor redColor];
+    [button setTitle:@"test" forState:UIControlStateNormal];
+    button.sp_acceptEventInterval = 1.0f;
+    [button addTarget:self action:@selector(logButtonTap) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    
     // Do any additional setup after loading the view, typically from a nib.
     
+}
+
+- (void)logButtonTap {
+    NSLog(@"UIButton tap");
 }
 
 
